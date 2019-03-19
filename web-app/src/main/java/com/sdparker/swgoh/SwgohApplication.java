342358fbe18d946a0e4b09b60b614719d356072e
sdparker <stephendparker@gmail.com>
2019-03-19 10:04:52 -0400

    initial commit

---
+++ web-app/src/main/java/com/sdparker/swgoh/SwgohApplication.java
@@ -0,0 +1,13 @@
+package com.sdparker.swgoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwgohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwgohApplication.class, args);
+	}
+
+}
---
+++ web-app/src/test/java/com/sdparker/swgoh/SwgohApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sdparker.swgoh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwgohApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
